31c27b76056b9f70a54eef13ca8f4be3fead8274
Luiz Pansarini <luiz.pansarini@uaubox.com.br>
2020-10-15 15:53:01 -0300

    connections.ts change

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,16 +1,15 @@
-import path from 'path'
+import knex from 'knex'
 import 'dotenv/config'
 
-module.exports = {
+const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.PG_HOST,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-  },
-  migrations: {
-    directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+    host: 'localhost',
+		user: 'postgres',
+		password: 'docker',
+		database: 'proffy',
   },
   useNullAsDefault: true
-}
+})
+
+export default db
