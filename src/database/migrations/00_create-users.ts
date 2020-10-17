8b73ef993819484b44fa55f1fca213ea72d8445b
Luiz Henrique <lpansarini@gmail.com>
2020-10-16 23:50:42 -0300

    changes on migrations

--- src/database/migrations/00_create-users.ts
+++ src/database/migrations/00_create-users.ts
@@ -5,7 +5,7 @@ export async function up(knex: Knex) {
         table.increments('id').primary();
         table.string('name').notNullable();
         table.string('email').notNullable();
-        table.string('password').notNullable();
+        table.string('password');
         table.string('avatar');
         table.string('whatsapp');
         table.string('bio');
